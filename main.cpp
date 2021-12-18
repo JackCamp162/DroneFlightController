edf4a727bb645ec0c69c473e28fbc20d29d06124
Jack <73950973+JackCamp162@users.noreply.github.com>
2021-12-18 12:39:12 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ Serial pc(USBTX, USBRX);
 
 int main()
 {
-    printf("This is the bare metal blinky example running on Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
+    printf("This is the bare metal blinking LED", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
 
     while (true)
     {
